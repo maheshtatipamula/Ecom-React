4701d01f2612f5094d5078f03abbfc3242ca468e
maheshtatipamula <mahesh.tatipamula43@gmail.com>
2023-11-15 21:34:13 +0530

    product details commit

--- src/features/productList/components/ProductsDetails.js
+++ src/features/productList/components/ProductsDetails.js
@@ -4,11 +4,12 @@ import { RadioGroup } from "@headlessui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 import {
   fetchSingleProductAsync,
   selectedProductDetails,
 } from "../productListSlice";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { selectLoggedInUser } from "../../auth/authSlice";
 import { addToCartAsync, selectAddCartErrors } from "../../cart/CartSlice";
 import {
@@ -177,161 +178,14 @@ export default function ProductDetails() {
             <div className="mt-4 lg:row-span-3 lg:mt-0">
               <h2 className="sr-only">Product information</h2>
               <p className="text-3xl tracking-tight text-gray-900">
-                {product.price}
+                price : ${product.price}
               </p>
 
-              {/* Reviews */}
-              <div className="mt-6">
-                <h3 className="sr-only">Reviews</h3>
-                <div className="flex items-center">
-                  <div className="flex items-center">
-                    {[0, 1, 2, 3, 4].map((rating) => (
-                      <StarIcon
-                        key={rating}
-                        className={classNames(
-                          product.ratings > rating
-                            ? "text-gray-900"
-                            : "text-gray-200",
-                          "h-5 w-5 flex-shrink-0"
-                        )}
-                        aria-hidden="true"
-                      />
-                    ))}
-                  </div>
-                  <p className="sr-only">{product.ratings} out of 5 stars</p>
-                </div>
-              </div>
-
               <form className="mt-10">
-                {/* Colors */}
-                <div>
-                  <h3 className="text-sm font-medium text-gray-900">Color</h3>
-
-                  <RadioGroup
-                    value={selectedColor}
-                    onChange={setSelectedColor}
-                    className="mt-4"
-                  >
-                    <RadioGroup.Label className="sr-only">
-                      Choose a color
-                    </RadioGroup.Label>
-                    <div className="flex items-center space-x-3">
-                      {colors &&
-                        colors.map((color) => (
-                          <RadioGroup.Option
-                            key={color.name}
-                            value={color}
-                            className={({ active, checked }) =>
-                              classNames(
-                                color.selectedClass,
-                                active && checked ? "ring ring-offset-1" : "",
-                                !active && checked ? "ring-2" : "",
-                                "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none"
-                              )
-                            }
-                          >
-                            <RadioGroup.Label as="span" className="sr-only">
-                              {color.name}
-                            </RadioGroup.Label>
-                            <span
-                              aria-hidden="true"
-                              className={classNames(
-                                color.class,
-                                "h-8 w-8 rounded-full border border-black border-opacity-10"
-                              )}
-                            />
-                          </RadioGroup.Option>
-                        ))}
-                    </div>
-                  </RadioGroup>
-                </div>
-
-                {/* Sizes */}
-                <div className="mt-10">
-                  <div className="flex items-center justify-between">
-                    <h3 className="text-sm font-medium text-gray-900">Size</h3>
-                    <a
-                      href="#"
-                      className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      Size guide
-                    </a>
-                  </div>
-
-                  <RadioGroup
-                    value={selectedSize}
-                    onChange={setSelectedSize}
-                    className="mt-4"
-                  >
-                    <RadioGroup.Label className="sr-only">
-                      Choose a size
-                    </RadioGroup.Label>
-                    <div className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
-                      {sizes &&
-                        sizes.map((size) => (
-                          <RadioGroup.Option
-                            key={size.name}
-                            value={size}
-                            disabled={!size.inStock}
-                            className={({ active }) =>
-                              classNames(
-                                size.inStock
-                                  ? "cursor-pointer bg-white text-gray-900 shadow-sm"
-                                  : "cursor-not-allowed bg-gray-50 text-gray-200",
-                                active ? "ring-2 ring-indigo-500" : "",
-                                "group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6"
-                              )
-                            }
-                          >
-                            {({ active, checked }) => (
-                              <>
-                                <RadioGroup.Label as="span">
-                                  {size.name}
-                                </RadioGroup.Label>
-                                {size.inStock ? (
-                                  <span
-                                    className={classNames(
-                                      active ? "border" : "border-2",
-                                      checked
-                                        ? "border-indigo-500"
-                                        : "border-transparent",
-                                      "pointer-events-none absolute -inset-px rounded-md"
-                                    )}
-                                    aria-hidden="true"
-                                  />
-                                ) : (
-                                  <span
-                                    aria-hidden="true"
-                                    className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"
-                                  >
-                                    <svg
-                                      className="absolute inset-0 h-full w-full stroke-2 text-gray-200"
-                                      viewBox="0 0 100 100"
-                                      preserveAspectRatio="none"
-                                      stroke="currentColor"
-                                    >
-                                      <line
-                                        x1={0}
-                                        y1={100}
-                                        x2={100}
-                                        y2={0}
-                                        vectorEffect="non-scaling-stroke"
-                                      />
-                                    </svg>
-                                  </span>
-                                )}
-                              </>
-                            )}
-                          </RadioGroup.Option>
-                        ))}
-                    </div>
-                  </RadioGroup>
-                </div>
-
                 <button
                   onClick={handleCart}
                   type="submit"
-                  className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="mt-5 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                   Add to bag
                 </button>
@@ -339,12 +193,18 @@ export default function ProductDetails() {
                 <button
                   onClick={handleWishlist}
                   type="submit"
-                  className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="mt-5 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                   {isProductWishlisted
                     ? "remove from wishlist"
                     : "Add to Wishlist"}
                 </button>
+                <Link
+                  to="/"
+                  className="mt-5 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  go back
+                </Link>
               </form>
             </div>
 
@@ -357,23 +217,6 @@ export default function ProductDetails() {
                   <p className="text-base text-gray-900">
                     {product.description}
                   </p>
-                </div>
-              </div>
-
-              <div className="mt-10">
-                <h3 className="text-sm font-medium text-gray-900">
-                  Highlights
-                </h3>
-
-                <div className="mt-4">
-                  <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
-                    {product.highlights &&
-                      product.highlights.map((highlight) => (
-                        <li key={highlight} className="text-gray-400">
-                          <span className="text-gray-600">{highlight}</span>
-                        </li>
-                      ))}
-                  </ul>
                 </div>
               </div>
 
--- src/features/user/components/Wishlist.js
+++ src/features/user/components/Wishlist.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast, Toaster } from "react-hot-toast";
 import { Link } from "react-router-dom";
+
 import {
   addToWishlistAsync,
   fetchWishlistAsync,
@@ -57,10 +58,11 @@ const Wishlist = () => {
 
   return (
     <div>
-      <div className="mx-auto h-auto  bg-white max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto h-auto p-4 bg-white max-w-7xl px-4 sm:px-6 lg:px-8">
         <h2 className="text-4xl my-4 pt-5 font-bold tracking-tight text-gray-900">
           Wishlist
         </h2>
+
         <div className="mt-8">
           <div className="flow-root">
             <ul
@@ -72,20 +74,26 @@ const Wishlist = () => {
                   key={item.id}
                   className="flex py-6 mb-3 bg-white-200 p-5 rounded-md  shadow-md"
                 >
-                  <div className="h-24 w-24 flex-shrink-0 overflow-hidden  rounded-md border border-gray-200">
-                    <img
-                      src={item.thumbnail}
-                      alt={item.title}
-                      className="h-full w-full object-cover object-center"
-                    />
-                  </div>
-                  <div className="ml-4">
-                    <h3 className="text-lg font-semibold text-gray-900">
-                      {item.title}
-                    </h3>
-                    <p className="text-gray-500">Price: ${item.price}</p>
-                    <p className="text-gray-500">Rating: {item.rating}</p>
-                  </div>
+                  <Link
+                    to={`/product-details/${item.id}`}
+                    key={item.id}
+                    className="flex"
+                  >
+                    <div className="h-24 w-24 flex-shrink-0 overflow-hidden  rounded-md border border-gray-200">
+                      <img
+                        src={item.thumbnail}
+                        alt={item.title}
+                        className="h-full w-full object-cover object-center"
+                      />
+                    </div>
+                    <div className="ml-4">
+                      <h3 className="text-lg font-semibold text-gray-900">
+                        {item.title}
+                      </h3>
+                      <p className="text-gray-500">Price: ${item.price}</p>
+                      <p className="text-gray-500">Rating: {item.rating}</p>
+                    </div>
+                  </Link>
                   <div className="flex ml-auto items-center">
                     <button
                       type="button"
@@ -100,6 +108,12 @@ const Wishlist = () => {
             </ul>
           </div>
         </div>
+        <Link
+          to="/"
+          className="block my-6 p-2 h-35 w-[180px] bg-blue-500 text-white text-center rounded-lg hover-bg-blue-600 transition duration-300"
+        >
+          go back
+        </Link>
       </div>
       <Toaster position="top-center" reverseOrder={false} />
     </div>
